196ee6f4ce3137fa9a289b1ecc4f0584663da8dd
TheSaviourEking <saviour.eking@outlook.com>
2024-03-01 02:26:44 +0100

    chore: create queries to examine the tables that I created in the previous phase

--- sql-practice/phase-3/query-2.sql
+++ sql-practice/phase-3/query-2.sql
@@ -1,2 +1,30 @@
 -- Find All the Toys for Hermione's cats
 -- Your code here
+--- THREE joins
+-- SELECT
+--   toys.name
+-- FROM
+--   toys
+--   JOIN cats ON (toys.cat_id = cats.id)
+--   JOIN cat_owners ON (cat_owners.cat_id = toys.cat_id)
+--   JOIN owners ON (owners.id = cat_owners.owner_id)
+-- WHERE
+--   owners.first_name = 'Hermione';
+
+-- TWO JOINS
+SELECT
+  toys.name
+FROM
+  toys
+  JOIN cats ON (toys.cat_id = cats.id)
+  JOIN cat_owners ON (
+    cat_owners.cat_id = toys.cat_id
+    AND cat_owners.owner_id = (
+      SELECT
+        id
+      FROM
+        owners
+      WHERE
+        owners.first_name = 'Hermione'
+    )
+  );
