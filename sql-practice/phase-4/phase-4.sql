01f9e8fb1192f36c2e9f2085b3af5aff7725cbf9
TheSaviourEking <saviour.eking@outlook.com>
2024-03-01 02:28:09 +0100

    chore: refactor the db schema so that when data is deleted, data that depends on it is also deleted

--- sql-practice/phase-4/phase-4.sql
+++ sql-practice/phase-4/phase-4.sql
@@ -1,8 +1,47 @@
-PRAGMA foreign_keys=on; -- Turns foreign key support in SQLite3 on
--- Your code here
+PRAGMA foreign_keys = ON;
 
-INSERT INTO owners
-  (first_name, last_name)
+-- Turns foreign key support in SQLite3 on
+-- Your code here
+DROP TABLE IF EXISTS cat_owners;
+
+DROP TABLE IF EXISTS toys;
+
+DROP TABLE IF EXISTS owners;
+
+DROP TABLE IF EXISTS cats;
+
+CREATE TABLE
+  owners (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    first_name text,
+    last_name text
+  );
+
+CREATE TABLE
+  cats (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name text,
+    birth_year INTEGER
+  );
+
+CREATE TABLE
+  cat_owners (
+    cat_id integer,
+    owner_id integer,
+    FOREIGN KEY (cat_id) REFERENCES cats (id) ON DELETE CASCADE,
+    FOREIGN KEY (owner_id) REFERENCES owners (id) ON DELETE CASCADE
+  );
+
+CREATE TABLE
+  toys (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name text,
+    cat_id INTEGER,
+    FOREIGN KEY (cat_id) REFERENCES cats (id) ON DELETE CASCADE
+  );
+
+INSERT INTO
+  owners (first_name, last_name)
 VALUES
   ('Nathan', 'Shanahan'),
   ('Joycelyn', 'Cummerata'),
@@ -12,8 +51,8 @@ VALUES
   ('Jonathan', 'Arbuckle'),
   ('Hermione', 'Granger');
 
-INSERT INTO cats
-  (name, birth_year)
+INSERT INTO
+  cats (name, birth_year)
 VALUES
   ('Smudge', 2014),
   ('Molly', 2015),
@@ -24,8 +63,8 @@ VALUES
   ('Garfield', 2009),
   ('Crookshanks', 2017);
 
-INSERT INTO cat_owners
-  (cat_id, owner_id)
+INSERT INTO
+  cat_owners (cat_id, owner_id)
 VALUES
   (1, 1),
   (1, 2),
@@ -37,8 +76,8 @@ VALUES
   (7, 6),
   (8, 7);
 
-INSERT INTO toys
-  (cat_id, name)
+INSERT INTO
+  toys (cat_id, name)
 VALUES
   (1, 'Catnip Mouse'),
   (2, 'Feather Wand'),
@@ -52,4 +91,5 @@ VALUES
   (8, 'Yarn');
 
 DELETE FROM cats
-WHERE cats.name = 'Smudge';
+WHERE
+  cats.name = 'Smudge';
