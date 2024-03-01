b1fff9225e310a1c04914d830fe63c0306392299
TheSaviourEking <saviour.eking@outlook.com>
2024-03-01 02:29:24 +0100

    chore: create queries using different SELECT clauses

--- sql-practice/phase-5/query-2.sql
+++ sql-practice/phase-5/query-2.sql
@@ -1,2 +1,11 @@
 -- Find the name and birth year of all the cats ordered by descending birth year
 -- Your code here
+SELECT DISTINCT
+  name,
+  birth_year
+FROM
+  cats
+-- GROUP BY
+--   name
+ORDER BY
+  birth_year DESC;
--- sql-practice/phase-5/query-3.sql
+++ sql-practice/phase-5/query-3.sql
@@ -1,2 +1,11 @@
 -- Find the name of the cats who have an owner whose first name begins with an "H"
 -- Your code here
+SELECT
+  cats.name
+FROM
+  cats
+  JOIN cat_owners ON (cat_owners.cat_id = cats.id)
+  JOIN owners ON (cat_owners.owner_id = owners.id)
+WHERE
+  -- owners.first_name like 'H%';
+  SUBSTR(owners.first_name, 1, 1) = 'H';
